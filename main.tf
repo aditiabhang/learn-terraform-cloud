40aa5842194492214f5a1b4f05b1ab019574db20
Aditi Abhang <aditi.abhang91@gmail.com>
2023-03-08 15:57:12 -0600

    working with key pairs

--- main.tf
+++ main.tf
@@ -5,6 +5,19 @@ provider "aws" {
 resource "aws_instance" "demo_instance" {
   ami           = "ami-006dcf34c09e50022"
   instance_type = var.instance_type
+  key_name      = var.my_key
+}
+
+# resource "aws_key_pair" "TF_key" {
+#   key_name = var.my_key
+#   public_key = tls_private_key.rsa.public_key_openssh
+
+# }
+
+# resource "tls_private_key" "rsa" {
+#   algorithm = "RSA"
+#   rsa_bits = 4096
+# }
 
   tags = {
     Name = var.instance_name
