4250d7ec0b78ab56433f2d4f26b9104bcca63b09
Aditi Abhang <aditi.abhang91@gmail.com>
2023-03-08 17:09:34 -0600

    working around s3 bucket logging

--- main.tf
+++ main.tf
@@ -13,15 +13,10 @@ resource "aws_instance" "demo_ec2_instance" {
 
 resource "aws_s3_bucket" "log_bucket" {
 bucket = "my-tf-log-bucket"
-acl    = "log-delivery-write"
+# acl    = "log-delivery-write"
 }
-
-resource "aws_s3_bucket" "b" {
-    bucket = "my-tf-test-bucket"
-    acl    = "private"
 
 logging {
     target_bucket = aws_s3_bucket.log_bucket.id
     target_prefix = "log/"
-}
 }
