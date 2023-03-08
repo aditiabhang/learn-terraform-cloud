ba42c754301ff1cd0fde04b22afa31c03f4b9a46
Aditi Abhang <aditi.abhang91@gmail.com>
2023-03-08 16:51:42 -0600

    changed aws account & changed ec2 instance name

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "demo_ec2" {
+resource "aws_instance" "demo_ec2_instance" {
   ami           = "ami-0557a15b87f6559cf"
   instance_type = var.instance_type
 
