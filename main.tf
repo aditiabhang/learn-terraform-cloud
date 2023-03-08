4d91696af4b157ba475c8c7f3d05c7b4b37d1ec6
Aditi Abhang <aditi.abhang91@gmail.com>
2023-03-08 15:58:46 -0600

    working with key pairs

--- main.tf
+++ main.tf
@@ -6,6 +6,11 @@ resource "aws_instance" "demo_instance" {
   ami           = "ami-006dcf34c09e50022"
   instance_type = var.instance_type
   key_name      = var.my_key
+
+    tags = {
+    Name = var.instance_name
+  }
+
 }
 
 # resource "aws_key_pair" "TF_key" {
@@ -19,7 +24,4 @@ resource "aws_instance" "demo_instance" {
 #   rsa_bits = 4096
 # }
 
-  tags = {
-    Name = var.instance_name
-  }
 
